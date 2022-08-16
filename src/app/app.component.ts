40b8dea41b02d7aebe195b41cdde1fb2b985e30a
Arie van Halem <arie@arievanhalem.nl>
2022-08-16 08:56:16 +0200

    Aanpassen genereer algor. en optimalisatie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subject, Subscription } from 'rxjs';
-import { scan, tap } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { scan, startWith, tap } from 'rxjs/operators';
 import { BLOCKSIZE, board, cell, SIZE, sudoku } from './sudoku'
 
 type viewState = {
@@ -22,11 +22,9 @@ type updateAction = (vm: viewState) => Partial<viewState>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
   public vm$: Observable<viewState>
-  public vm: viewState
   public updateVm = new Subject<updateAction>()
-  public subscription = new Subscription()
   public level = 35
   public levelOptions = [
     { name: "Makkie", value: 62 },
@@ -39,9 +37,10 @@ export class AppComponent implements OnInit, OnDestroy {
 
   constructor() {
     this.vm$ = this.updateVm.pipe(
+      startWith((vm: viewState) => ({ board: sudoku.getEmptyBoard() })),
       scan((state, action) => {
-        const resets = {check_solution: false, create_message: undefined, solve_message: undefined}
-        const nwState = { ...state, ...resets , ...action(state) }
+        const resets = { check_solution: false, create_message: undefined, solve_message: undefined }
+        const nwState = { ...state, ...resets, ...action(state) }
         return { ...nwState, displayBoard: sudoku.getDisplayBoard(nwState.board) }
       }, {
         board: undefined,
@@ -51,25 +50,11 @@ export class AppComponent implements OnInit, OnDestroy {
         solution_string: undefined,
         solution: undefined,
         check_solution: false,
-        create_message: undefined, 
+        create_message: undefined,
         solve_message: undefined
       }),
       tap(console.log)
     )
-  }
-
-  ngOnInit(): void {
-    this.subscription.add(
-      this.vm$.subscribe(
-        _vm => { this.vm = _vm }
-      )
-    )
-
-    this.updateVm.next((vm: viewState) => ({ board: sudoku.getEmptyBoard() }));
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe()
   }
 
   setValue(r: number, c: number, n: number) {
@@ -125,11 +110,11 @@ export class AppComponent implements OnInit, OnDestroy {
       }
 
       const nwBoard = sudoku.deserialize(s.solution_string)
-      const emptyIdxs = s.board.cells.map((v, i) => ({v: v.value, i})).filter(c => !c.v).map(({i}) => i)
-      if(emptyIdxs.length > 0) {
+      const emptyIdxs = s.board.cells.map((v, i) => ({ v: v.value, i })).filter(c => !c.v).map(({ i }) => i)
+      if (emptyIdxs.length > 0) {
         const emptyIdx = emptyIdxs[Math.floor(Math.random() * emptyIdxs.length)]
         const hint = nwBoard.cells[emptyIdx].value
-        var board = {cells: [].concat(s.board.cells)}
+        var board = { cells: [].concat(s.board.cells) }
         board.cells[emptyIdx].value = hint
 
         return {
@@ -191,22 +176,27 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   generate() {
-    const _timestamp = new Date()
     this.updateVm.next((_: viewState) => ({
       board: sudoku.getEmptyBoard(),
       create_message: `Er wordt een puzzel gemaakt. Doel is ${81 - this.level} lege vakjes.`
     }))
-    const [board, solution] = sudoku.generate(81 - this.level)
-    const solution_cells = sudoku.getDisplayBoard(sudoku.deserialize(solution))
+    setTimeout(() =>
+      this.updateVm.next((_: viewState) => {
+        const _timestamp = new Date()
+        const [board, solution] = sudoku.generate(81 - this.level)
+        const solution_cells = sudoku.getDisplayBoard(sudoku.deserialize(solution))
 
-    this.updateVm.next((_: viewState) => ({
-      board,
-      solution: solution_cells,
-      solution_string: solution,
-      create_message: `Er is een puzzle gevonden met. Met ${board.cells.filter(c => !c.value).length} 
-        lege vakjes van de ${81 - this.level} gevraagde. 
-        Tijdsduur ${new Date((new Date()).getTime() - _timestamp.getTime()).toISOString().replace('1970-01-01T','')}`
-    }));
+        return {
+          board,
+          solution: solution_cells,
+          solution_string: solution,
+          create_message: `Er is een puzzle gevonden met. Met ${board.cells.filter(c => !c.value).length} 
+          lege vakjes van de ${81 - this.level} gevraagde. 
+          Tijdsduur ${new Date((new Date()).getTime() - _timestamp.getTime()).toISOString().replace('1970-01-01T', '')}`
+        }
+      })
+    , 10);
+
   }
 
   alloptions() {
--- src/app/sudoku.ts
+++ src/app/sudoku.ts
@@ -16,6 +16,11 @@ export type board = {
 // PRIVATE HELPER FUNCTIONS //
 const range = (l: number) => [...Array(l).keys()]
 
+const shuffle = (source: any[]) => source
+  .map(v => ({ v, sort: Math.random() }))
+  .sort((a, b) => a.sort - b.sort)
+  .map(({ v }) => v)
+
 const getCellIndex = (row: number, column: number) => (row * SIZE) + column
 
 const getRowColumnIndex = (idx: number) => [
@@ -58,6 +63,20 @@ const stringify = (b: number[]) => {
   return b.map(c => c ? c.toString() : '_').join('')
 }
 
+const getEmptyBoard = () => ({
+  cells: range(BOARDSIZE)
+    .map(n => ({
+      value: undefined,
+      options: [],
+      frozen: false
+    }))
+} as board)
+
+const getRandomSolution = () =>
+  solve(getEmptyBoard().cells.map(c => c.value), 1, true)[0]
+    .split('')
+    .map(c => c === '_' ? undefined : parseInt(c))
+
 const solve = (b: number[], K: number = 1, randomize: boolean = false): string[] => {
   if (b.length !== BOARDSIZE) {
     return [] as string[]
@@ -81,28 +100,73 @@ const solve = (b: number[], K: number = 1, randomize: boolean = false): string[]
   }, [])
 }
 
-const getEmptyBoard = () => ({
-  cells: range(BOARDSIZE)
-    .map(n => ({
-      value: undefined,
-      options: [],
-      frozen: false
-    }))
-} as board)
+const getShuffeledIndexes = (solution: number[], K: number) => {
+  let factor = K - 20 / 40
+  factor = factor < 0 ? 0 : factor > 1 ? 1 : factor
+  const weights = [-0.5,-0.5,-0.65,-0.85,0,0.15,0.35,0.5,0.5].map(w => (w * factor) + 1)
+  const rowWeights = shuffle(weights)
+  const colWeights = shuffle(weights)
+  const numWeights = shuffle(weights)
 
-const shuffle = (source: any[]) => source
-  .map(v => ({ v, sort: Math.random() }))
-  .sort((a, b) => a.sort - b.sort)
-  .map(({ v }) => v)
+  return range(BOARDSIZE).map(i => {
+    const [r,c] = getRowColumnIndex(i)
+    const w = rowWeights[r] * colWeights[c] * numWeights[solution[i]-1] * Math.random();
+    return {i, w}
+  }).sort((a,b) => a.w > b.w ? 1 : -1).map(o => o.i)
+}
 
-const getRandomElement = (source: any[]) =>
-  source[Math.floor(Math.random() * source.length)]
+const punchHoles = (solution: number[], K: number, indexes: number[])  => {
+  const result = [].concat(solution)
+  let idxs = [].concat(indexes)
+
+  result[idxs[0]] = undefined
+  idxs = idxs.slice(1)
+  let count = K - 1
+  while (count > 0 && idxs.length > 0) {
+    let numSol = 0
+    const idx = idxs[0]
+    idxs = idxs.slice(1)
+    const currentNumber = result[idx]
+    result[idx] = undefined
+    numSol = solve(result, 1).length
+    if (numSol === 0) {
+      // er is geen oplossing voor dit grid
+      result[idx] = currentNumber
+      console.log(`${new Date().toISOString()} Zoekstap (geen oplossing): ${idxs.length}/81 ${K - count}/${K}`)
+    } else {
+      // heeft een van de andere nummers 1 oplossing, dan zijn er meerdere oplossingen
+      numSol = 0
+      let numbers = range(SIZE).map(n => n + 1).filter(n => n !== currentNumber)
+      while (numSol === 0 && numbers.length > 0) {
+        const testNumber = numbers[0]
+        numbers = numbers.slice(1)
+        if (isValidOnIndex(result, idx, testNumber)) {
+          const testOptie = [].concat(result)
+          testOptie[idx] = testNumber
+          numSol = solve(testOptie, 1).length
+          console.log(`${new Date().toISOString()} Zoekstap index ${idx}, nummer ${testNumber}, oplossingen ${numSol}`)
+        }
+      }
+
+      if (numSol !== 0) {
+        result[idx] = currentNumber
+        console.log(`${new Date().toISOString()} Zoekstap (meerdere oplossingen): ${idxs.length}/81 ${K - count}/${K}`)
+      } else {
+        count--
+        console.log(`${new Date().toISOString()} Zoekstap (oplossing): ${idxs.length}/81 ${K - count}/${K}`)
+      }
+    }
+  }
+
+  console.log(`${new Date().toISOString()} Puzzel gevonden: ${result.filter(v => !v).length}/${K}`)
+  return result
+}
 
 // API //
 export const sudoku = {
   serialize: (b: board) => stringify(b.cells.map(c => c.value)),
 
-  deserialize: (s: string) => s.length !== BOARDSIZE ? getEmptyBoard() : ({
+  deserialize: (s: string) => (s || '').length !== BOARDSIZE ? getEmptyBoard() : ({
     cells: s.split('').map(c => ({
       value: c === '_' ? undefined : parseInt(c),
       options: [],
@@ -163,56 +227,19 @@ export const sudoku = {
 
   generate: (K: number): [board, string] => {
     console.log(`${new Date().toISOString()} Start genereren`)
-    // initiele oplossing (random solution)
-    const result = solve(getEmptyBoard().cells.map(c => c.value), 1, true)[0].split('').map(c => c === '_' ? undefined : parseInt(c))
-    const solution = [].concat(result)
-    console.log(`${new Date().toISOString()} Grid gevonden: ${stringify(result)}`)
+    // find random solution
+    const solution = getRandomSolution()
+    console.log(`${new Date().toISOString()} Grid gevonden: ${stringify(solution)}`)
 
-    // bepaal de volgorde van de indxen en maak de eerste cell leeg
-    let idxs = shuffle(range(81))
-    result[idxs[0]] = undefined
-    idxs = idxs.slice(1)
+    // find order of punching
+    const idxs = getShuffeledIndexes(solution, K)
 
-    let count = K - 1
-    while (count > 0 && idxs.length > 0) {
-      let numSol = 0
-      const idx = idxs[0] 
-      idxs = idxs.slice(1)
-      const currentNumber = result[idx]
-      result[idx] = undefined
-      numSol = solve(result, 1).length
-      if (numSol === 0) {
-        // er is geen oplossing voor dit grid
-        result[idx] = currentNumber
-        console.log(`${new Date().toISOString()} Zoekstap (geen oplossing): ${idxs.length}/81 ${K-count}/${K}`)
-      } else {
-        // heeft een van de andere nummers 1 oplossing, dan zijn er meerdere oplossingen
-        numSol = 0
-        let numbers = range(SIZE).map(n => n + 1).filter(n => n !== currentNumber)
-        while (numSol === 0 && numbers.length > 0) {
-          const testNumber = numbers[0]
-          numbers = numbers.slice(1)
-          if (isValidOnIndex(result, idx, testNumber)) {
-            const testOptie = [].concat(result)
-            testOptie[idx] = testNumber
-            numSol = solve(testOptie, 1).length
-            console.log(`${new Date().toISOString()} Zoekstap index ${idx}, nummer ${testNumber}, oplossingen ${numSol}`)
-          }
-        }
-
-        if (numSol !== 0) {
-          result[idx] = currentNumber
-          console.log(`${new Date().toISOString()} Zoekstap (meerdere oplossingen): ${idxs.length}/81 ${K-count}/${K}`)
-        } else {
-          count--
-          console.log(`${new Date().toISOString()} Zoekstap (oplossing): ${idxs.length}/81 ${K-count}/${K}`)
-        }
-      }
-    }
-
-    console.log(`${new Date().toISOString()} Puzzel gevonden: ${result.filter(v => !v).length}/${K}`)
+    // punch the holes
+    const puzzle = punchHoles(solution, K, idxs)
+    
+    // return result
     return [{
-      cells: result.map(c => ({
+      cells: puzzle.map(c => ({
         value: c,
         options: [],
         frozen: c ? true : false
